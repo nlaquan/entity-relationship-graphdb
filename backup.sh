1f224122182b8081c4f004afdd2a8c098000bdf1
quannla <anhquan.nguyenle@gmail.com>
2020-02-28 19:13:48 +0700

    remove unused file

--- backup.sh
+++
@@ -1,6 +0,0 @@
-docker run --name=neo4j-backup -d \
-  -v backup-neo4j:/data \
-  --env NEO4J_AUTH=neo4j/test \
-  --env NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
-  neo4j:enterprise /bin/bash -c "neo4j-admin backup --from=192.168.25.102:6362 \
-    --backup-dir=/data --name=graph.db-2020/02/03"
--- script.sh
+++
@@ -1,9 +0,0 @@
-docker run \
-    --name neo4j-xyz \
-    -p7474:7474 -p7687:7687 -p7473:7474 \
-    -d \
-    --env=NEO4J_dbms_backup_enabled=true \
-    --env=NEO4J_dbms_backup_address=0.0.0.0:6362 \
-    --env NEO4J_AUTH=neo4j/test \
-    --env NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
-    neo4j:3.5.9-enterprise
